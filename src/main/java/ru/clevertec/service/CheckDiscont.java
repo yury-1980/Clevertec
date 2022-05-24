79c3865b5b65e2c881a0ee185283aa04206b3cd5
yury-1980 <58135208+yury-1980@users.noreply.github.com>
2022-05-24 13:34:26 +0300

    Разбил на методы, Stream - передел
    Переделал stream_API под новые методы

--- src/main/java/ru/clevertec/service/CheckDiscont.java
+++ src/main/java/ru/clevertec/service/CheckDiscont.java
@@ -7,17 +7,22 @@ import ru.clevertec.model.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CheckDiscont {
 
-    public static List<Check> checkList;
+    // Массив товара исходный
+    static Map<Integer, Product> products = Reader.getProduct();
+
+    // Массив предпологаемой покупки
+    static List<Product> masProducts = new ArrayList<>();
+
+    // Итоговый массив - чек
+    public static List<Check> checks = new ArrayList<>();
 
     public static void masProducts(String[] args) {
-        Map<Integer, Product> products = Reader.getProduct();// Массив товара исходный
-        List<Product> masProducts = new ArrayList<>();// Массив предпологаемой покупки
         String[] str = new String[2];// id и кол-во
+        String regex_0 = "^([1-9]\\d?|100)$"; // id товара
+        String regex_1 = "^([1-9]|1[0-9]|20)$";// Колличество товара
 
         for (String arg : args) {
             str = arg.split("-");
@@ -27,8 +32,8 @@ public class CheckDiscont {
 
             if (!(str[0].equals("card"))) {
 
-                if ((!str[0].matches("^([1-9]\\d?|100)$"))
-                        || (!str[1].matches("^([1-9]|1[0-9]|20)$"))) {
+                if ((!str[0].matches(regex_0))
+                        || (!str[1].matches(regex_1))) {
                     continue;
                 }
                 try {
@@ -40,76 +45,98 @@ public class CheckDiscont {
                     num = 0;
                 }
 
-                if (products.containsKey(id)) {// Проверка наличия товара в магазине
-
-                    if (!(masProducts.contains(products.get(id)))) {
-                        masProducts.add(products.get(id));// Добавление товара в чек
-                        /* Кол-во товара */
-                        masProducts.get(masProducts.size() - 1).setNumber(num);
-                    } else {
-
-                        for (Product m : masProducts) {
-
-                            if (m.getId() == products.get(id).getId()) {
-                                // Прибавление кол-ва товара в чек, к существующему
-                                m.setNumber(m.getNumber() + num);
-                            }
-                        }
-                    }
-
+                // Проверка наличия товара в магазине
+                if (products.containsKey(id)) {
+                    addProduct(id, num);
                 } else {
                     System.out.println("Product not found!!!");
                 }
-            } else {
+            } else {// При предъявлении карты
                 break;
             }
         }
 
+        // Общая стоимость всего товара по скидке, добавление в чек
+        setDiscountTotal(str);
 
-        int productNumDiscont = 0;// Кол-во дисконтного товара
-        List<Check> checks = new ArrayList<>();
+        // Общая стоимость всего нескидочного товара, добавление в чек
+        setTotalCostOfAllNon_discountItem();
 
-        // Подсчёт кол-ва дисконтных товаров
-        productNumDiscont = masProducts.stream().filter(x -> x.isDiscount())
-                .map(x -> x.getNumber()).mapToInt(x -> x).sum();
-
-
-        int finalProductNumDiscont = productNumDiscont;
-        String[] finalStr = str;
-
-        masProducts.stream()
-                .filter(p -> p.isDiscount() && (finalProductNumDiscont > 5)
-                        && finalStr[0].equals("card"))
-                .peek(p -> {
-            checks.add(new Check(p.getNumber(), p.getTitle(), p.getPrice(),
-                    p.getNumber() * p.getPrice() * 0.9));
-
-            // Общая стоимость всего скидочного товара
-            Check.setDiscountTotal((Check.getDiscountTotal()
-                    + p.getNumber() * p.getPrice() * 0.9));
-        }).collect(Collectors.toList());
-
-        masProducts.stream()
-                .filter(p -> !p.isDiscount())
-                .peek(p -> {
-                    checks.add(new Check(p.getNumber(), p.getTitle(), p.getPrice(),
-                            p.getNumber() * p.getPrice()));
-                    // Общая стоимость всего нескидочного товара
-                    Check.setTotalCostOfAllNon_discountItem(Check.getTotalCostOfAllNon_discountItem()
-                            + p.getNumber() * p.getPrice());
-                }).collect(Collectors.toList());
-
-        // Общая стоимость всего товара без скидки
-        Check.setSumTotal(masProducts.stream().map(p -> p.getNumber() * p.getPrice())
-                .mapToDouble(p -> p).sum());
-
+        // Полная стоимость всего товара (без скидки)
+        setSumTotal();
 
         // Сумма всей скидки
+        setDiscontSum();
+        // Окончательная сумма (с учётом скидок)
+        setFinalAmount();
+    }
+
+    static void addProduct(int id, int num) {
+        if (!(masProducts.contains(products.get(id)))) {
+            // Добавление товара в  предпологаемую покупку
+            masProducts.add(products.get(id));
+            /* Кол-во товара */
+            masProducts.get(masProducts.size() - 1).setNumber(num);
+        } else {
+
+            // Прибавление кол-ва товара к существующему, предпологаемую покупку
+            masProducts.stream()
+                    .filter(p -> p.getId() == products.get(id).getId())
+                    .peek(p -> p.setNumber(p.getNumber() + num)).count();
+        }
+    }
+
+    //    Общая сумма 1-го типа
+    static double totalSumOfOneItem(Product product) {
+        return product.getNumber() * product.getPrice();
+    }
+
+    // Подсчёт кол-ва дисконтных товаров
+    static int getProductNumDiscont(List<Product> masProducts) {
+        int productNumDiscont = masProducts.stream().filter(x -> x.isDiscount())
+                .map(x -> x.getNumber()).mapToInt(x -> x).sum();
+        return productNumDiscont;
+    }
+
+    static void setDiscountTotal(String[] str) {
+        boolean isDiscountCard = getProductNumDiscont(masProducts) > 5 && str[0].equals(
+                "card");
+
+        // Общая стоимость всего товара по скидке, добавление в чек
+        Check.setDiscountTotal(masProducts.stream()
+                .filter(p -> p.isDiscount() && isDiscountCard)
+                .peek(p ->
+                        checks.add(new Check(p.getNumber(), p.getTitle(), p.getPrice(),
+                                totalSumOfOneItem(p) * 0.9))
+                ).map(p -> (totalSumOfOneItem(p) * 0.9))
+                .mapToDouble(p -> p).sum());
+    }
+
+    // Общая стоимость всего нескидочного товара, добавление в чек
+    static void setTotalCostOfAllNon_discountItem() {
+        Check.setTotalCostOfAllNon_discountItem(masProducts.stream()
+                .filter(p -> !p.isDiscount())
+                .peek(p ->
+                        checks.add(new Check(p.getNumber(), p.getTitle(), p.getPrice(),
+                                totalSumOfOneItem(p)))
+                ).map(p -> totalSumOfOneItem(p))
+                .mapToDouble(p -> p).sum());
+    }
+
+    // Полная стоимость всего товара (без скидки)
+    static void setSumTotal() {
+        Check.setSumTotal(masProducts.stream().map(p -> totalSumOfOneItem(p))
+                .mapToDouble(p -> p).sum());
+    }
+
+    // Сумма всей скидки
+    static void setDiscontSum() {
         Check.setDiscontSum(Check.getSumTotal() - Check.getTotalCostOfAllNon_discountItem()
                 - Check.getDiscountTotal());
-        // Окончательная сумма
-        Check.setFinalAmount(Check.getDiscountTotal() + Check.getTotalCostOfAllNon_discountItem());
+    }
 
-        checkList = checks;
+    // Окончательная сумма
+    static void setFinalAmount() {
+        Check.setFinalAmount(Check.getDiscountTotal() + Check.getTotalCostOfAllNon_discountItem());
     }
 }
